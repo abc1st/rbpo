2079e609e973293585cd104421b376693c204bec
abc1st <dakyg7@yandex.ru>
2024-12-14 23:42:18 +0300

    Логин вместо почты

--- src/main/java/ru/mtuci/babok/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/ru/mtuci/babok/service/impl/UserDetailsServiceImpl.java
@@ -16,8 +16,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        ApplicationUser user = userRepository.findByEmail(email)
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        ApplicationUser user = userRepository.findByLogin(login)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
         return UserDetailsImpl.fromApplicationUser(user);
     }
