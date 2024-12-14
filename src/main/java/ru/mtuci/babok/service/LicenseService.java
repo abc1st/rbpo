1b6b95367c4076659668103d9d802aa404b3d757
abc1st <dakyg7@yandex.ru>
2024-12-14 23:49:34 +0300

    Лицензии

--- src/main/java/ru/mtuci/babok/service/LicenseService.java
+++ src/main/java/ru/mtuci/babok/service/LicenseService.java
@@ -1,4 +1,18 @@
 package ru.mtuci.babok.service;
 
+import ru.mtuci.babok.model.LicenseActivate;
+import ru.mtuci.babok.request.LicenseRequest;
+
+import java.util.List;
+
 public interface LicenseService {
+    LicenseActivate createLicense(
+        Long CreatorId, int device_count
+        );
+
+    LicenseActivate save(LicenseRequest request);
+
+    List<LicenseActivate> getAll();
+
+    void delete(Long id);
 }
--- src/main/java/ru/mtuci/babok/service/impl/LicenseServiceImpl.java
+++ src/main/java/ru/mtuci/babok/service/impl/LicenseServiceImpl.java
@@ -1,4 +1,9 @@
 package ru.mtuci.babok.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class LicenseServiceImpl {
 }
