4442048c7928f3c90dbd5bcf4672b2b4e471575b
abc1st <dakyg7@yandex.ru>
2024-12-15 00:48:29 +0300

    Для лицензии

--- src/main/java/ru/mtuci/babok/controller/LicenseController.java
+++ src/main/java/ru/mtuci/babok/controller/LicenseController.java
@@ -1,5 +1,11 @@
 package ru.mtuci.babok.controller;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import org.hibernate.annotations.ManyToAny;
+import ru.mtuci.babok.model.ApplicationUser;
 import ru.mtuci.babok.service.impl.LicenseServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,4 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @PreAuthorize("*hasRole('ADMIN')")
 public class LicenseController {
     private final LicenseServiceImpl licenseService;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "userId")
+    private ApplicationUser user;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "CreatorId")
+    private ApplicationUser creator;
 }
