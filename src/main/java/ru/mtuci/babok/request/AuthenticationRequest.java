581cb3110b76f2b976070d00be56872773cab14c
abc1st <dakyg7@yandex.ru>
2024-12-20 19:17:29 +0300

    Убраны не нужные аннотации

--- src/main/java/ru/mtuci/babok/request/AuthenticationRequest.java
+++ src/main/java/ru/mtuci/babok/request/AuthenticationRequest.java
@@ -1,11 +1,8 @@
 package ru.mtuci.babok.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class AuthenticationRequest {
-    private String login;
-    private String password;
+    private String login, password;
 }
