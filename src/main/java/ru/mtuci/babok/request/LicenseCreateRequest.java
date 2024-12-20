c775bb8027d23d49f4aec981516427ed7afab37f
abc1st <dakyg7@yandex.ru>
2024-12-20 19:19:59 +0300

    Измененные названия и убраны не нужные

--- src/main/java/ru/mtuci/babok/request/LicenseCreateRequest.java
+++ src/main/java/ru/mtuci/babok/request/LicenseCreateRequest.java
@@ -2,13 +2,9 @@ package ru.mtuci.babok.request;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
-public class LicenseCreateRequest
-{
-    private Long CreatorId;
+public class LicenseCreateRequest {
+    private Long productId, ownerId, licenseTypeId;
     private Integer device_count;
-    private String current_device;
-    private LocalDateTime lifeTime;
+    private Long duration;
 }
