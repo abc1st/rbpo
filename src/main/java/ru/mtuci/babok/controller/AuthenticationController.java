2453558ddb8dda9ef93e5fde157a999a41c9f7ab
abc1st <dakyg7@yandex.ru>
2024-12-20 19:15:52 +0300

    Нормальная обработка запроса на вход

--- src/main/java/ru/mtuci/babok/controller/AuthenticationController.java
+++ src/main/java/ru/mtuci/babok/controller/AuthenticationController.java
@@ -1,27 +1,27 @@
 package ru.mtuci.babok.controller;
 
+import ru.mtuci.babok.configuration.JwtTokenProvider;
+import ru.mtuci.babok.model.ApplicationUser;
+import ru.mtuci.babok.model.AuthenticationResponse;
+import ru.mtuci.babok.repository.UserRepository;
+import ru.mtuci.babok.request.AuthenticationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.mtuci.babok.configuration.JwtTokenProvider;
-import ru.mtuci.babok.model.ApplicationUser;
-import ru.mtuci.babok.request.AuthenticationRequest;
-import ru.mtuci.babok.model.AuthenticationResponse;
-import ru.mtuci.babok.repository.UserRepository;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/auth/login")
 @RequiredArgsConstructor
 public class AuthenticationController {
-
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
@@ -31,22 +31,17 @@ public class AuthenticationController {
         try {
             String login = request.getLogin();
 
-            ApplicationUser user = userRepository.findByLogin(login)
-                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(login, request.getPassword())
+            );
 
-            authenticationManager
-                    .authenticate(
-                            new UsernamePasswordAuthenticationToken(
-                                    login, request.getPassword())
-                    );
+            ApplicationUser user = userRepository.findByLogin(login).orElseThrow(() -> new UsernameNotFoundException("user not found"));
 
-            String token = jwtTokenProvider
-                    .createToken(login, user.getRole().getGrantedAuthorities());
+            String token = jwtTokenProvider.createToken(login, user.getRole().getGrantedAuthorities());
 
-            return ResponseEntity.ok(new AuthenticationResponse(login, token));
-        } catch (AuthenticationException ex) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body("Invalid email or password");
+            return ResponseEntity.ok(new AuthenticationResponse(token, login));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid login or password");
         }
     }
 }
