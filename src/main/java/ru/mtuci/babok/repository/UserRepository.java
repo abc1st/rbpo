fe8862456dc49f4e33c7020c7e4d5cdfd5bc52ce
abc1st <dakyg7@yandex.ru>
2024-12-20 19:25:07 +0300

    Убраны не нужные импорты

--- src/main/java/ru/mtuci/babok/repository/UserRepository.java
+++ src/main/java/ru/mtuci/babok/repository/UserRepository.java
@@ -1,13 +1,10 @@
 package ru.mtuci.babok.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.mtuci.babok.model.ApplicationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-
-@Repository
 public interface UserRepository extends JpaRepository<ApplicationUser, Long> {
     Optional<ApplicationUser> findByLogin(String login);
 }
