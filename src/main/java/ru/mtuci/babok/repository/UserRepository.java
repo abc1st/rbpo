c0d716cc175237bd289b133bc6f6fe2f105ae551
abc1st <dakyg7@yandex.ru>
2025-03-02 11:40:41 +0300

    Добавлено поле для поиска по почте

--- src/main/java/ru/mtuci/babok/repository/UserRepository.java
+++ src/main/java/ru/mtuci/babok/repository/UserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<ApplicationUser, Long> {
     Optional<ApplicationUser> findByLogin(String login);
+    Optional<ApplicationUser> findByEmail(String email);
 }
