7058cb3d0028a1b0a7459984b6fa0d6399deb2a5
abc1st <dakyg7@yandex.ru>
2025-04-06 17:45:13 +0300

    Fix Аннулирование всех активных сессий для того же пользователя и устройства

--- src/main/java/ru/mtuci/babok/repository/UserSessionRepository.java
+++ src/main/java/ru/mtuci/babok/repository/UserSessionRepository.java
@@ -1,6 +1,7 @@
 package ru.mtuci.babok.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.mtuci.babok.model.SessionStatus;
 import ru.mtuci.babok.model.UserSession;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.Optional;
 public interface UserSessionRepository extends JpaRepository<UserSession, Long> {
     Optional<UserSession> findByRefreshToken(String refreshToken);
     List<UserSession> findByEmail(String email);
+    Optional<UserSession> findByAccessToken(String accessToken);
+    List<UserSession> findByEmailAndDeviceIdAndStatus(String email, String deviceId, SessionStatus status);
 }
