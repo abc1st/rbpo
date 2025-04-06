a49a167fb73849aefd854163f73d63563e1b6adf
abc1st <dakyg7@yandex.ru>
2025-04-06 17:43:46 +0300

    Fix Если сессия не активна, запрос отклоняется

--- src/main/java/ru/mtuci/babok/configuration/SecurityConfig.java
+++ src/main/java/ru/mtuci/babok/configuration/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.authentication.AuthenticationManager;
+import ru.mtuci.babok.repository.UserSessionRepository;
 
 @Configuration
 @EnableMethodSecurity
@@ -21,10 +22,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 public class SecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
     private final UserDetailsService userDetailsService;
+    private UserSessionRepository userSessionRepository;
 
     @Bean
     public JwtTokenFilter jwtTokenFilter() {
-        return new JwtTokenFilter(jwtTokenProvider, userDetailsService);
+        return new JwtTokenFilter(jwtTokenProvider, userDetailsService, userSessionRepository);
     }
 
     @Bean
